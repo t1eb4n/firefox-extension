9401f33e615054ce3eb43535bd7b8dc4d84b76d5
Christian South <t1eb4n@gmail.com>
2022-06-29 00:59:13 -0400

    Misc updates before Vue/TS rewrite

--- js/site_specific/news.ycombinator.com.js
+++ js/site_specific/news.ycombinator.com.js
@@ -2,5 +2,18 @@
 
 (function() {
   /** Make links open in a new tab **/
-  Array.from(document.getElementsByClassName('titlelink')).forEach(elem => elem.target = '_blank');
+  document.querySelectorAll('.titlelink').forEach(elem => {
+    /** Open in new tab **/
+    elem.target = '_blank';
+
+    const url = new URL(elem.href);
+
+    /** Add favicons **/
+    const favicon   = document.createElement('img');
+    favicon.style   = 'height: 16px; width: 16px; margin-right: 5px; float: left;';
+    favicon.src     = `${url.origin}/favicon.ico`;
+    favicon.onerror = () => favicon.src = browser.runtime.getURL('imgs/icon.512.png');
+
+    elem.prepend(favicon);
+  });
 })();
